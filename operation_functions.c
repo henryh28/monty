6e6e89445e86ddd3429ae6342a20d97b84c56ff1
Henry Hsu <h_hsu817@yahoo.com>
2017-12-22 01:17:23 +0000

    Adds input checks

--- operation_functions.c
+++ operation_functions.c
@@ -7,9 +7,31 @@
  * @line_number: Current line number in monty script file
  */
 
-void op_push(stack_t **stack, unsigned int line_number __attribute__((unused)))
+void op_push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new;
+	unsigned int size;
+	int number;
+
+	if (global.value == NULL)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	size = atoi(global.value) <= 0 ? 1 : 0;
+	number = abs(atoi(global.value));
+
+	while (number > 0)
+	{
+		number /= 10;
+		size++;
+	}
+	if (size != strlen(global.value))
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
